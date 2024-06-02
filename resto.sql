81cd3595124b73cf1b4490ba88b9f6ea093a7963
FathanRasyidi <162400625+FathanRasyidi@users.noreply.github.com>
2024-06-02 20:39:20 +0700

    Add files via upload

---
+++ resto.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 02 Jun 2024 pada 15.38
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `resto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `restoran`
+--
+-- Kesalahan membaca struktur untuk tabel resto.restoran: #1932 - Table &#039;resto.restoran&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel resto.restoran: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `resto`.`restoran`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `review`
+--
+-- Kesalahan membaca struktur untuk tabel resto.review: #1932 - Table &#039;resto.review&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel resto.review: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `resto`.`review`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+-- Kesalahan membaca struktur untuk tabel resto.user: #1932 - Table &#039;resto.user&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel resto.user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `resto`.`user`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
